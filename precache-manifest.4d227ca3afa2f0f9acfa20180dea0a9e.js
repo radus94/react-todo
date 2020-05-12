86c48c4e3c819db185c0d76b6bb8df7bc1db61fa
rsirghe <rsirghe@noction.com>
2020-05-12 18:11:52 +0300

    Updates

---
+++ precache-manifest.4d227ca3afa2f0f9acfa20180dea0a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6c3bde98f65f5abe19bf3486dba5d5",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2ae60192e0791133d6ba",
+    "url": "/react-todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a9386645cb859e36ad2",
+    "url": "/react-todo/static/js/2.8325cccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.8325cccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae60192e0791133d6ba",
+    "url": "/react-todo/static/js/main.b038ad46.chunk.js"
+  },
+  {
+    "revision": "a7fb28e01dcc192be58f",
+    "url": "/react-todo/static/js/runtime-main.59b49ada.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
