0dafa590e7e7e2101687f993b991c4b2d2993543
Radu Sirghe <38857993+radus94@users.noreply.github.com>
2020-05-14 15:43:32 +0300

    Added todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { useState, useReducer } from 'react'
+import reducer from './reducer'
+
+
+function Todo({todo, index, completeTodo, deleteTodo}) {
+    return (
+        <li
+            className="todo"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <label>
+                <input
+                    type="checkbox"
+                    defaultChecked={todo.isCompleted}
+                    onClick={() => completeTodo(index)}
+                />
+                <span>{todo.title}</span>
+            </label>
+            <i className="material-icons red-text" onClick={() => deleteTodo(index)}>delete</i>
+        </li>
+    )
 }
 
-export default App;
+function TodoForm({addTodo}) {
+    const [value, setValue] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!value) return;
+        addTodo(value);
+        setValue('');
+    }
+    return (
+        <form className="input-field" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add todo..."
+                onChange={e=>setValue(e.target.value)} />
+            <label>Todo name</label>
+        </form>
+    )
+}
+
+export default function App() {
+
+  const [state] = useReducer(reducer, JSON.parse(localStorage.getItem('TodoStorage')) || [])
+
+  const [todos, setTodos] = useState(state);
+
+  const addTodo = (title, isCompleted) => {
+      const newTodos = [...todos, { title , isCompleted}];
+      setTodos(newTodos)
+      localStorage.setItem('TodoStorage', JSON.stringify(newTodos))
+  };
+
+  const completeTodo = index => {
+      const newTodos = [...todos];
+      newTodos[index].isCompleted = newTodos[index].isCompleted !== true;
+      setTodos(newTodos)
+      localStorage.setItem('TodoStorage', JSON.stringify(newTodos))
+  }
+
+  const deleteTodo = index => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos)
+      localStorage.setItem('TodoStorage', JSON.stringify(newTodos))
+  }
+
+  return (
+      <div className="container">
+          <h1>Todo app</h1>
+          <TodoForm addTodo={addTodo} />
+          <div>
+              {todos.map((todo, index) => (
+                  <Todo
+                      key={index}
+                      index={index}
+                      todo={todo}
+                      completeTodo={completeTodo}
+                      deleteTodo={deleteTodo}
+                  />
+              ))}
+          </div>
+      </div>
+  )
+}
